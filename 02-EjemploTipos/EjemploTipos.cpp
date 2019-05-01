8defd629769999df6777ca13836f0b65eab0e23d
ivanvillacorta <ivanvillacorta@outlook.es>
2019-05-01 15:24:52 -0300

    Trabajo practico numero 2

---
+++ 02-EjemploTipos/EjemploTipos.cpp
@@ -0,0 +1,28 @@
+/*  
+Trabajo practico "EjemploTipos"
+Ivan enrique villacorta 
+30/04/2019
+*/
+
+#include <string>
+#include <iostream>
+#include <assert.h>
+
+
+int main(){
+
+int a{5};
+double b{6.5};
+bool c{true};
+char d[6]="TAREA";
+unsigned e;
+
+
+
+assert(a+a==10);   //suma de enteros
+assert(b+b==13);    //Suma de reales
+assert(c==true);    //Comparacion booleana
+assert(d!="TAREA");  //Lectura de una cadena de caracteres
+assert(e!=a);       //Comparacion de un valor aleatorio con una constante
+
+}
